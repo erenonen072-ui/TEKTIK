dcc9ca9066c575a13aee03bea8bcaa3a16331c01
erenonen072-ui <erenonen072@gmail.com>
2026-08-08 14:45:37 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = "tektik-v1";
+
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(FILES_TO_CACHE))
+    );
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            )
+        )
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(cached => cached || fetch(event.request))
+    );
+});
